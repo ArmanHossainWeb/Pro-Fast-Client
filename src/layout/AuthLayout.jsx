477a24bc1bc4bf38a7ab8535abd4039efda39ae1
ArmanHossain04 <armanhossain0477@gmail.com>
2025-07-29 11:17:49 +0600

    modified: login page modified

--- src/layout/AuthLayout.jsx
+++ src/layout/AuthLayout.jsx
@@ -5,13 +5,13 @@ import ProFastLogo from "../pages/shared/proFastLogo/ProFastLogo";
 
 const AuthLayout = () => {
   return (
-    <div className="bg-base-200 min-h-screen">
+    <div className="bg-base-300 min-h-screen">
       <div>
         <ProFastLogo></ProFastLogo>
       </div>
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <img src={AuthImg} className="max-w-sm rounded-lg shadow-2xl" />
-        <div>
+        <img src={AuthImg} className="max-w-lg rounded-lg shadow-2xl" />
+        <div className="w-full md:w-1/5  lg:w-1/3 ">
           <Outlet></Outlet>
         </div>
       </div>
--- src/pages/Authentication/Login/Login.jsx
+++ src/pages/Authentication/Login/Login.jsx
@@ -1,19 +1,44 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { FcGoogle } from "react-icons/fc";
 
 const Login = () => {
+  const {register, handleSubmit, formState: {errors}} = useForm()
+
+  const onSubmit = (data) => {
+    console.log(data);
+  }
+   
   return (
-    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+    <div className="max-w-lg shrink-0 shadow-2xl">
       <div className="card-body">
-        <fieldset className="fieldset">
+        <form onSubmit={handleSubmit(onSubmit)} className="fieldset">
+          {/* email  */}
           <label className="label">Email</label>
-          <input type="email" className="input" placeholder="Email" />
+          <input type="email" {...register('email')} className="input" placeholder="Email" />
+          {/* password */}
           <label className="label">Password</label>
-          <input type="password" className="input" placeholder="Password" />
+          <input type="password" {...register('password',  {required:true, minLength:6})} className="input" placeholder="Password" />
+
+          {
+            errors.password?.type === "required" && <p className="text-red-500"> password is required</p>
+          }
+          {
+            errors.password?.type === "minLength" && <p className="text-red-500">password must be 6 charecter oo longer</p>
+          }
           <div>
             <a className="link link-hover">Forgot password?</a>
           </div>
           <button className="btn btn-neutral mt-4">Login</button>
-        </fieldset>
+
+           <div className="divider">OR</div> 
+
+          {/* Google */}
+          <button className="btn bg-white text-black border-[#e5e5e5]">
+            <FcGoogle />
+            Login with Google
+          </button>
+        </form>
       </div>
     </div>
   );
